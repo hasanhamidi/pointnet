757d1e299bfdd82c3a7080c6a547a4472cb3d32a
hasanhamidi <hamidi3031@gmail.com>
2022-06-02 16:00:03 +0430

    1

--- utils/trainer_class.py
+++ utils/trainer_class.py
@@ -35,7 +35,8 @@ class Trainer():
     def train_one_epoch(self,epoch_number=0):
         batch_length = len(self.train_data_loader)
         loss_train = []
-        for i, data in enumerate(self.train_data_loader, 0):
+        iterator = tqdm(self.train_data_loader, total=batch_length)
+        for i, data in iterator:
                 points, target = data
                 points = points.transpose(2, 1)
                 points, target = points.cuda(), target.cuda()
@@ -52,7 +53,7 @@ class Trainer():
                 self.optimizer.step()
                 pred_choice = pred.data.max(1)[1]
                 correct = pred_choice.eq(target.data).cpu().sum()
-                print('[%d: %d/%d] train loss: %f accuracy: %f' % ( epoch_number, i, batch_length, loss.item(), correct.item()/float(self.batch_size * 2500)))
+                iterator.set_description('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch_number, i, batch_length, loss.item(), correct.item()/float(self.batch_size * 2500)))
                 loss_train.append(loss.item())
         return np.mean(loss_train)
     def validation_one_epoch(self,epoch_number = 0):
